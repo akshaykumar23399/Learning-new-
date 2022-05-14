c304231603b990c7bc9f5326835860f230f9e57f
Акшау Кумар <akshaykumar23399@gmail.com>
2022-05-14 23:57:36 +0530

    Dependency upgraded

--- demo/src/main/java/com/test/demo/DemotestApplication.java
+++ demo/src/main/java/com/test/demo/DemotestApplication.java
@@ -1,10 +1,9 @@
 package com.test.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemotestApplication {
